5db5e374a240244aca32437f71113ab78e70c629
Marcin Dekier <marcin.dekier@appunite.com>
2018-10-21 15:12:48 +0200

    remove library

--- marcin-dekier/plugins/artyom.js
+++
@@ -1,9 +0,0 @@
-  import Vue from 'vue'
-  import Artyom from 'artyom.js'
-if (process.browser) {
-  let artyom = new Artyom()
-  Vue.use(artyom)
-}
-// require styles
-// import 'swiper/dist/css/swiper.css'
-
